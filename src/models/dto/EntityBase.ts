c967e62b0e852d3a6114282c1de18077404ab94f
BenoitSafari <gregory.michalak.192@gmail.com>
2024-11-17 23:10:51 +0100

    feat(editor): Fallback value for model selector

--- src/models/dto/EntityBase.ts
+++ src/models/dto/EntityBase.ts
@@ -3,3 +3,23 @@ export interface EntityBase<T extends string | number = any> {
     createdAt: Date;
     updatedAt: Date;
 }
+
+export class EntityBaseHelper {
+    public static build<T extends EntityBase>(entity: T): T {
+        return {
+            ...entity,
+            createdAt: new Date(entity.createdAt),
+            updatedAt: new Date(entity.updatedAt),
+        };
+    }
+
+    public static getNewest(entities: EntityBase[]): EntityBase | undefined {
+        return entities.find(
+            entity => entity.createdAt.getTime() === Math.max(...entities.map(e => e.createdAt.getTime())),
+        );
+    }
+
+    public static getById(entities: EntityBase[], id: string | number | undefined): EntityBase | undefined {
+        return entities.find(entity => String(entity.id) === String(id));
+    }
+}
