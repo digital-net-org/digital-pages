f1d2b49a7e43b0c5c9d263d3a6180e79b847213e
horameus <ludo.hartmann@gmail.com>
2025-01-30 18:16:19 +0100

    feature(TableEdit): adding patch and delete

--- src/pages/views/[id]/page.tsx
+++ src/pages/views/[id]/page.tsx
@@ -1,6 +1,6 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Edit, Icon, Loader } from '@digital-net/react-digital-ui';
-import { useGetById, useSchema } from '@digital-net/react-digital-client';
+import { useGetById, useSchema, usePatch, useDelete } from '@digital-net/react-digital-client';
 import React from 'react';
 import type { ViewModel } from '@/models';
 import EntityForm from '@digital-net/react-digital-ui/components/Form/EntityForm/EntityForm';
@@ -9,24 +9,48 @@ export default function ViewPage() {
     const { id } = useParams();
     const { entity, isQuerying, invalidateQuery: invalidate } = useGetById<ViewModel>('view', id);
     const { schema, isLoading: isSchemaLoading } = useSchema('/view');
+    const navigate = useNavigate();
+
+    const isLoading = React.useMemo(() => isSchemaLoading || isQuerying, [isSchemaLoading, isQuerying]);
+
+    const { patch } = usePatch<ViewModel>('/view', {
+        onSuccess: async () => await invalidate(),
+    });
+
+    const { delete: _delete } = useDelete('/view', {
+        onSuccess: async () => await invalidate(),
+    });
 
     const [formData, setFormData] = React.useState<Partial<ViewModel>>({});
 
-    const handlePatch = () => {
-        console.log('Submitting:', formData);
-    };
+    const handlePatch = React.useCallback(
+        async () => {
+            if (!id || isLoading) {
+                return;
+            }
+            patch(id, formData);
+        },
+        [id, isLoading, patch, formData],
+    );
 
-    const handleDelete = () => {
-        console.log('Delete');
-    };
+    const handleDelete = React.useCallback(
+        async () => {
+            if (!id || isLoading) {
+                return;
+            }
+            _delete(id);
+            navigate('/views');
+        },
+        [id, isLoading, _delete, navigate],
+    );
 
     return (
         <div>
             <Edit
                 renderName={() => id}
                 actions={[
-                    { icon: Icon.FloppyIcon, action: handlePatch },
-                    { icon: Icon.TrashIcon, action: handleDelete },
+                    { icon: Icon.FloppyIcon, action: handlePatch, disabled: isLoading },
+                    { icon: Icon.TrashIcon, action: handleDelete, disabled: isLoading },
                 ]}
             >
                 {isQuerying && !entity ? <Loader /> : null}
