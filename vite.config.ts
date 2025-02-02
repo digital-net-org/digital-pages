3c3ead17f51a27960becd60a8cffdd08df31fe53
BenoitSafari <gregory.michalak.192@gmail.com>
2025-02-02 21:15:24 +0100

    refactor(core): Re worked production architecture

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import { build } from './config/build';
-import { resolveConstants } from './config/constant';
+import { resolveConstants } from './vite.constants';
 
 export default defineConfig({
     define: {
@@ -22,7 +21,23 @@ export default defineConfig({
             '@digital-net': resolve(__dirname, 'packages/digital-net/packages/'),
         },
     },
-    build,
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, '..', 'index.html'),
+                lib: resolve(__dirname, '..', 'src/index.tsx'),
+            },
+            output: {
+                compact: true,
+                strict: true,
+                format: 'es',
+                sourcemap: false,
+                entryFileNames: '[hash].js',
+                chunkFileNames: 'lib/[hash].js',
+            },
+        },
+    },
     test: {
         globals: true,
         environment: 'jsdom',
