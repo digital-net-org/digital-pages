0e701299608e80ce14ea3a50804bd1d68f1bbea5
horameus <ludo.hartmann@gmail.com>
2025-02-10 00:34:49 +0100

    feature(TableEdit): useEntityForm to handle un- & controlled fields

--- src/pages/views/[id]/page.tsx
+++ src/pages/views/[id]/page.tsx
@@ -1,63 +1,33 @@
-import { useParams, useNavigate } from 'react-router-dom';
 import { Edit, Icon, Loader } from '@digital-net/react-digital-ui';
-import { useGetById, useSchema, usePatch, useDelete } from '@digital-net/react-digital-client';
 import React from 'react';
 import type { ViewModel } from '@/models';
 import EntityForm from '@digital-net/react-digital-ui/components/Form/EntityForm/EntityForm';
+import useEntityForm from '@digital-net/react-digital-ui/components/Form/EntityForm/useEntityForm';
 
 export default function ViewPage() {
-    const { id } = useParams();
-    const { entity, isQuerying, invalidateQuery: invalidate } = useGetById<ViewModel>('view', id);
-    const { schema, isLoading: isSchemaLoading } = useSchema('/view');
-    const navigate = useNavigate();
-
-    const isLoading = React.useMemo(() => isSchemaLoading || isQuerying, [isSchemaLoading, isQuerying]);
-
-    const { patch } = usePatch<ViewModel>('/view', {
-        onSuccess: async () => await invalidate(),
-    });
-
-    const { delete: _delete } = useDelete('/view', {
-        onSuccess: async () => await invalidate(),
-    });
-
-    const [formData, setFormData] = React.useState<Partial<ViewModel>>({});
-
-    const handlePatch = React.useCallback(
-        async () => {
-            if (!id || isLoading) {
-                return;
-            }
-            console.log('patching', id, formData);
-            patch(id, formData);
-        },
-        [id, isLoading, patch, formData],
-    );
-
-    const handleDelete = React.useCallback(
-        async () => {
-            if (!id || isLoading) {
-                return;
-            }
-            _delete(id);
-            navigate('/views');
-        },
-        [id, isLoading, _delete, navigate],
-    );
+    const {
+        id,
+        entity,
+        schema,
+        isLoading,
+        isQuerying,
+        handleDelete,
+        handlePatch,
+    } = useEntityForm<ViewModel>('view');
 
     return (
         <div>
             <Edit
                 renderName={() => id}
                 actions={[
-                    { icon: Icon.FloppyIcon, action: handlePatch, disabled: isLoading, formId: id },
+                    { icon: Icon.FloppyIcon, disabled: isLoading, formId: id },
                     { icon: Icon.TrashIcon, action: handleDelete, disabled: isLoading },
                 ]}
             >
                 {isQuerying && !entity ? <Loader /> : null}
                 {!isQuerying && entity
                     ? (
-                            <EntityForm schema={schema} entity={entity} onSubmitData={setFormData} id={id} />
+                            <EntityForm schema={schema} defaultEntity={entity} onSubmit={handlePatch} id={id} />
                         )
                     : <h2>not found</h2>}
             </Edit>
