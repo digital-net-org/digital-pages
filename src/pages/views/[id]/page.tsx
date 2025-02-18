f1322d9f7c0d2647a2438307a1a666eb23689399
horameus <ludo.hartmann@gmail.com>
2025-02-18 23:03:50 +0100

    feature(components): page handling EntityPage

--- src/pages/views/[id]/page.tsx
+++ src/pages/views/[id]/page.tsx
@@ -1,25 +1,11 @@
 import type { ViewModel } from '@digital-lib/dto';
-import { Edit, Icon, Loader, EntityForm, useEntityForm } from '@digital-lib/react-digital-ui';
+import { useEntityForm, EntityPage } from '@digital-lib/react-digital-ui';
 
 export default function ViewPage() {
     const { payload, setPayload, id, schema, isLoading, isQuerying, handleDelete, handlePatch } =
         useEntityForm<ViewModel>('view', 'views');
 
     return (
-        <div>
-            <Edit
-                renderName={() => id}
-                actions={[
-                    { icon: Icon.FloppyIcon, disabled: isLoading, formId: id },
-                    { icon: Icon.TrashIcon, action: handleDelete, disabled: isLoading },
-                ]}
-            >
-                {isQuerying || !payload ? (
-                    <Loader />
-                ) : (
-                    <EntityForm id={id} schema={schema} value={payload} onChange={setPayload} onSubmit={handlePatch} />
-                )}
-            </Edit>
-        </div>
+        <EntityPage {...{ id, isLoading, isQuerying, handleDelete, handlePatch, schema, payload, setPayload }} />
     );
 }
