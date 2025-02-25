3ca81f213bebab7e1eaae84d46b37668071035be
horameus <ludo.hartmann@gmail.com>
2025-02-25 23:00:46 +0100

    feature(component): EntityPage handling loading & mutation

--- src/pages/views/[id]/page.tsx
+++ src/pages/views/[id]/page.tsx
@@ -2,12 +2,12 @@ import type { ViewModel } from '@digital-lib/dto';
 import { useEntityForm, EntityPage } from '@digital-lib/react-digital-ui';
 
 export default function ViewPage() {
-    const { payload, setPayload, id, schema, isLoading, isQuerying, handleDelete, handlePatch } =
+    const { payload, setPayload, id, schema, isLoading, handleDelete, handlePatch, isMutated } =
         useEntityForm<ViewModel>('view', 'views');
 
     return (
         <EntityPage
-            {...{ id, isLoading, isQuerying, schema, payload, setPayload }}
+            {...{ id, isLoading, isMutated, schema, payload, setPayload }}
             onSave={handlePatch}
             onDelete={handleDelete}
             title={payload?.title}
