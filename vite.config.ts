bb9299b8da90dc7566a182bc85068e87c6405c29
BenoitSafari <gregory.michalak.192@gmail.com>
2025-03-26 23:48:51 +0100

    feature(vite): ViteBuilder helper

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,20 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
-import { resolveConstants } from './vite.constants';
+import { ViteBuilder } from './packages/digital-lib/vite';
+import { constants } from './vite.constants';
 
-export default defineConfig({
-    define: {
-        global: 'globalThis',
-        ...resolveConstants(),
+export default ViteBuilder.buildConfig({
+    constants,
+    port: 3045,
+    alias: {
+        '@': resolve(__dirname, 'src/'),
+        '@digital-lib': resolve(__dirname, 'packages/digital-lib/packages/'),
     },
-    server: {
-        port: 3045,
+    assets: {
+        'styles.puck.css': resolve(__dirname, 'assets/styles.puck.css'),
+        'styles.theme.css': resolve(__dirname, 'assets/styles.theme.css'),
+        'favicon.ico': resolve(__dirname, 'assets/favicon.ico'),
     },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src/'),
-            '@digital-lib': resolve(__dirname, 'packages/digital-lib/packages/'),
-        },
-    },
-    build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                lib: resolve(__dirname, 'src/index.tsx'),
-                'styles.puck.css': resolve(__dirname, 'assets/styles.puck.css'),
-                'styles.theme.css': resolve(__dirname, 'assets/styles.theme.css'),
-                'favicon.ico': resolve(__dirname, 'assets/favicon.ico'),
-            },
-            output: {
-                compact: true,
-                strict: true,
-                format: 'es',
-                sourcemap: false,
-                entryFileNames: '[hash].js',
-                chunkFileNames: '[hash].js',
-                assetFileNames: 'assets/[name][extname]',
-            },
-        },
-    },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-    },
-    plugins: [react(), checker({ typescript: true })],
 });
--- vite.constants.ts
+++ vite.constants.ts
@@ -2,7 +2,7 @@ import { loadEnv } from 'vite';
 
 const env = loadEnv('', process.cwd(), '');
 
-const constants = {
+export const constants = {
     APP_VERSION: process.env.npm_package_version,
     APP_DOCUMENT_NAME: 'Safari-Digital',
     APP_ERROR_PREFIX: 'DIGITAL_ERROR:',
@@ -14,10 +14,3 @@ const constants = {
     APP_PATH_FRAMES: '/frames',
     DIGITAL_API_URL: env.DIGITAL_API_URL,
 };
-
-export function resolveConstants() {
-    return Object.entries(constants).reduce((acc, [key, value]) => {
-        acc[key] = JSON.stringify(value);
-        return acc;
-    }, {});
-}
