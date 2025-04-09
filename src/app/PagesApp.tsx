65759c25726362129dd73b0b54b796dd59917cbd
BenoitSafari <gregory.michalak.192@gmail.com>
2025-04-10 01:13:55 +0200

    Digital-lib adaptations

--- src/app/PagesApp.tsx
+++ src/app/PagesApp.tsx
@@ -4,7 +4,6 @@ import { App } from '@digital-lib/react-digital';
 export default function PagesApp({ children }: React.PropsWithChildren) {
     /* TODO: 
         - Add parameter access to Puck schema upload/selection
-        - Move react-digital-puck to pages app
     */
-    return <App>{children}</App>;
+    return <App renderSettings={{}}>{children}</App>;
 }
